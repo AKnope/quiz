b97309351a042682e4d00ff62c3e2acebe1ebcc3
AKnope <artursknope2@gmail.com>
2021-12-15 10:25:35 +0200

    first

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+const questions = {
+    0:{
+        question: "When is christmas eve?",
+        "A": 26,
+        "B": 22,
+        "C": 24,
+        "D": 25,
+        "correctAnswer":"C"
+    },
+    1:{
+        question: "when is christmas eve?",
+        "A": 26,
+        "B": 22,
+        "C": 24,
+        "D": 25,
+        "correctAnswer":"C"
+    },
+    2:{
+        question: "when is christmas eve?",
+        "A": 26,
+        "B": 22,
+        "C": 24,
+        "D": 25,
+        "correctAnswer":"C"
+    },
+}
+console.log(questions[0]["question"])
+console.log(questions[1]["question"])
+console.log(questions[2]["question"])
